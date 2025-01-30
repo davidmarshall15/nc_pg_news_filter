79c4840c984a128c0fe0bc0e40caa58ac4b9e9a3
davidmarshall15 <davidmarshall15@users.noreply.github.com>
2025-01-30 17:46:58 +0000

    Formatting

--- cr_oc_news_filter_func.sql
+++ cr_oc_news_filter_func.sql
@@ -2,50 +2,50 @@ CREATE OR REPLACE FUNCTION oc_news_filter_func()
                            RETURNS trigger AS
 $BODY$
 DECLARE
-	dom_count int;
+    dom_count int;
     art_count int;
 BEGIN
-	SELECT count(*)
-	into   dom_count
+    SELECT count(*)
+    into   dom_count
     FROM   oc_news_filter
     WHERE  LOWER(NEW.url) LIKE '%' || LOWER(sub_domain) || '%'
-	AND    inc_filter;
+    AND    inc_filter;
 
-	if dom_count>0 then
+    if dom_count>0 then
 	
-	    SELECT count(*)
-		INTO   art_count
-	    FROM   oc_news_filter
-	    WHERE  LOWER(NEW.url) LIKE '%' || LOWER(sub_domain) || '%'
-	    AND   (LOWER(NEW.title) LIKE '%' || LOWER(txt_filter) || '%'
-	    OR     LOWER(NEW.body) LIKE '%' || LOWER(txt_filter) || '%')
-	    AND    inc_filter;
-	    if art_count<1 then
-	        NEW.unread := false;
-	    end if;
-	
+        SELECT count(*)
+        INTO   art_count
+        FROM   oc_news_filter
+        WHERE  LOWER(NEW.url) LIKE '%' || LOWER(sub_domain) || '%'
+        AND   (LOWER(NEW.title) LIKE '%' || LOWER(txt_filter) || '%'
+        OR     LOWER(NEW.body) LIKE '%' || LOWER(txt_filter) || '%')
+        AND    inc_filter;
+        if art_count<1 then
+            NEW.unread := false;
+        end if;
+
     end if;
 
-	SELECT count(*)
-	into   dom_count
+    SELECT count(*)
+    into   dom_count
     FROM   oc_news_filter
     WHERE  LOWER(NEW.url) LIKE '%' || LOWER(sub_domain) || '%'
-	AND    NOT inc_filter;
+    AND    NOT inc_filter;
 
-	if dom_count>0 then
-	    SELECT count(*)
-		INTO   art_count
-	    FROM   oc_news_filter
-	    WHERE  LOWER(NEW.url) LIKE '%' || LOWER(sub_domain) || '%'
-	    AND   (LOWER(NEW.title) LIKE '%' || LOWER(txt_filter) || '%'
-	    OR     LOWER(NEW.body) LIKE '%' || LOWER(txt_filter) || '%')
-	    AND    NOT inc_filter;
-	    if art_count>0 then
-	        NEW.unread := false;
-	    end if;
-	
+    if dom_count>0 then
+        SELECT count(*)
+        INTO   art_count
+        FROM   oc_news_filter
+        WHERE  LOWER(NEW.url) LIKE '%' || LOWER(sub_domain) || '%'
+        AND   (LOWER(NEW.title) LIKE '%' || LOWER(txt_filter) || '%'
+        OR     LOWER(NEW.body) LIKE '%' || LOWER(txt_filter) || '%')
+        AND    NOT inc_filter;
+        if art_count>0 then
+            NEW.unread := false;
+        end if;
+        
     end if;
-  RETURN NEW;
+    RETURN NEW;
 END;
 $BODY$
 LANGUAGE plpgsql;
